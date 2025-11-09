c3c35ac265aba280091c292ab2c36008d759b821
Gaurav jaiswal <gauravjayswal17082002@gmail.com>
2025-11-09 21:49:04 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Hero = () => {
+    const VideoRef =useRef();
+    useEffect(()=>{
+        if(VideoRef.current) VideoRef.current.playbackRate=2;
+
+    },[])
   return (
     <section id='hero'>
         <div>
-            
+            <h1> Macbook Pro</h1>
+            <img src='/public/title.png' />
+
         </div>
+        <video ref={VideoRef} src='/public/videos/hero.mp4' autoPlay muted playsInline/>
+        <button>
+            Buy
+        </button>
+        <p> From ₹79,999 or ₹6,999 for 12 months</p>
     </section>
   )
 }
