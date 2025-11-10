c98c24f9482f716d8d4012fa196346279141bd0e
Gaurav jaiswal <gauravjayswal17082002@gmail.com>
2025-11-10 18:57:25 +0530

    created features sectionn

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,13 @@ const useMacbookStore = create((set) =>({
 
     color:'#2e2c2e',
     setColor :(color)=> set({color}),
+    
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
 
-        scale:0.08,
-        setScale:(scale)=> set({scale}),
-        reset:()=> set({color : '#2e2c2e',scale:0.08}),
+    scale:0.08,
+    setScale:(scale)=> set({scale}),
+    reset:()=> set({color : '#2e2c2e',scale:0.08,texture: '/videos/feature-1.mp4'}),
 
     
 }))
